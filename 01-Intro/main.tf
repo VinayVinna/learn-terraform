66f75c16c84dd2f7ccb501940c85dc88951ed64b
user <vinaygoudmaduri@gmail.com>
2026-06-25 11:39:11 +0530

    training commit

--- 01-Intro/main.tf
+++ 01-Intro/main.tf
@@ -10,6 +10,12 @@ variable "x" {
   default = 20
 }
 
+# List Variable
+
+variable "y" {
+  default = [1, "abc", 0.5, true]
+}
+#List can have a combination of types also
 
 
 # Print variable on output
@@ -20,4 +26,8 @@ output "x" {
 #If our value is just a variable then var.x, if it is the combination of strings then "${var.x}"
 output "x1" {
   value = "value of x - ${var.x}"
+}
+
+output "y"{
+  value = var.y[2]
 }
