266e050581ac1ad71a3a98d71a09c3e55c170e1f
user <vinaygoudmaduri@gmail.com>
2026-06-25 11:32:47 +0530

    training commit

--- 01-Intro/main.tf
+++ 01-Intro/main.tf
@@ -3,3 +3,21 @@ resource "local_file" "foo" {
   content  = "Hello World"
   filename = "/tmp/foo.bar"
 }
+
+
+# Normal Variable
+variable "x" {
+  default = 20
+}
+
+
+
+# Print variable on output
+output "x" {
+  value = var.x
+}
+
+#If our value is just a variable then var.x, if it is the combination of strings then "${var.x}"
+output "x1" {
+  value = "value of x - "${var.x}"
+}
